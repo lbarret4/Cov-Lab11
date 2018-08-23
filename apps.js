bdb5cd44cc99983f950d16a630138713a14a3b52
lbarret4 <llewellynbar4@gmail.com>
2018-08-23 03:25:38 -0400

    Adds li, changes color of clicked li item and removes double clicked li

--- apps.js
+++ apps.js
@@ -1,41 +1,49 @@
 $(document).ready(function () {
 
     let textField;
+    var div = $('<div></div>' ).appendTo('body');
+    var ul = $('<ul></ul>').appendTo('body');
+
+    // Disables submit button.
+    $('#btnSubmit').prop('disabled','disabled');
+
+    // Adds alert windows and list creation functionality to submit button when clicked.
     $('#btnSubmit').click(function (e) {
-        // e.preventDefault()
+ 
         window.alert('The submit button was pressed.');
         textField = $('input[type=\'text\']').val();
         window.alert(textField);
         e.preventDefault();
 
-    });
-  
-    $('#btnSubmit').prop('disabled','disabled');
-
-    var div = $('<div></div>' ).appendTo('body');
-    
-    $('input[type=\'text\']').on('change',function(){
-   
-        $('#btnSubmit').prop('disabled',false);
-
-        $('#btnSubmit').one('click',function (e) {
-           
-            var h2 = $(`<h2>${textField}</h2>`).appendTo(div);
-            h2.on('mouseover',function(){
-                h2.css({
-                    backgroundColor: '#00ffff',
-                    borderRadius: '2px',
-                    borderStyle:'dashed'
-                });
-            })
-
+        //Add list item with text from textbox and changes color of text when list item is clicked.
+        let li = $(`<li>${textField}</li>`).appendTo(ul);
+        li.click(function(){
+            let color= randomColor();
+            li.css("color",color);
         });
 
+        //Removes list item that was doubled clicked.
+        li.dblclick(function(){
+            li.remove();
+        });
+        
+        
+
+    });
+  
+    
+    // Enables submit button once input is typed in text box.
+    $('input[type=\'text\']').on('change',function(){
+           $('#btnSubmit').prop('disabled',false);
     });
 
+   
 
-
-
+    //Return a random color in RGB format.
+    function randomColor() {
+        let [red, green, blue] = [Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255), Math.ceil(Math.random() * 255)];
+        return `rgb(${red},${green},${blue})`;
+    }
 
 
 });
