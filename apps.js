992a6fe78b5caf75c437f3bdb7b6f0e6074cfc79
lbarret4 <llewellynbar4@gmail.com>
2018-08-22 23:19:50 -0400

    Added  event handler for form submit and button press

--- apps.js
+++ apps.js
@@ -1,9 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#btnSubmit').click(function(){
+    $('#btnSubmit').click(function (e) {
+        // e.preventDefault()
+        window.alert('The submit button was pressed.');
+        let textField = $('input[type=\'text\'').val();
+        // console.log(textField);
+        // window.alert(textField);
 
-        alert('The submit button was pressed.');
-    })
+    });
+
+    $('form').on("submit", function () {
+
+        let textField = $('input[type=\'text\'').val();
+        window.alert(textField);
+        
+    
+
+    });
+
+
+
+
+
+
+
+
 
 });
 
